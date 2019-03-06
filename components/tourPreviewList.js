d33daf292facb43670e60d6a2e8da3d6f8b382dc
snzew <sitanzewde@snzews-MacBook-Pro.local>
2019-03-06 09:24:07 +0100

    first commit

---
+++ components/tourPreviewList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import uuidv1 from "uuid";
+import { tours } from '../data';
+import TourPreviewListItem from './tourPreviewListItem';
+
+const TourPreviewList = () => (
+   tours.map(item => (
+      <TourPreviewListItem headline = {item.headline} duration={item.duration} price={item.price} rating={item.rating} key={uuidv1()} />
+  ))
+)
+
+export default TourPreviewList;
+
+
---
+++ components/tourPreviewListItem.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export default class TourPreviewListItem extends Component {
+
+  render() {
+    return(
+      <View style={styles.viewContainer}>
+        <Image source={require("../assets/images/robot-dev.png")}></Image>
+        <View style={styles.textContainer}>
+          <Text style={styles.tourHeadline}>{this.props.headline}</Text>
+          <Text style={styles.smallText}>{this.props.duration}</Text>
+          <Text style={styles.smallText}>{this.props.price}</Text>
+          <View style={styles.ratingContainer}>
+            <Text style={styles.rating}>{this.props.rating}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+};
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    backgroundColor: 'lightgrey',
+    margin: 20,
+    shadowColor: 'lightgrey',
+    shadowOffset: { width: 3, height: 5 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+  },
+  textContainer: {
+    flex: 1,
+    flexWrap: 'wrap',
+    padding: 10,
+  },
+  tourHeadline: {
+    flex: 1,
+    flexWrap: 'wrap',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  smallText: {
+    color: 'black',
+    fontWeight: '100',
+    fontSize: 10,
+  },
+  tourPreviewImage: {
+    flex: 1,
+    width:10,
+    height: 50,
+    resizeMode: 'contain',
+  },
+  ratingContainer: {
+
+  },
+  rating:{
+    marginTop: 10,
+    color: 'black',
+    fontWeight: '100',
+    fontSize: 10
+  }
+});
