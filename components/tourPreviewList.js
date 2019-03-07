01b475be943064239ed0071955e4c29e9f0420b0
snzew <sitanzewde@snzews-MacBook-Pro.local>
2019-03-07 09:45:24 +0100

    edit style from tourPreview

--- components/tourPreviewList.js
+++ components/tourPreviewList.js
@@ -3,9 +3,15 @@ import uuidv1 from "uuid";
 import { tours } from '../data';
 import TourPreviewListItem from './TourPreviewListItem';
 
+
+const ratingNum = Math.floor(Math.random() * 5 )
+
+generateRandomNumer = () => {
+  const ratingNum = Math.floor(Math.random() * 5 + 1 )
+}
 const TourPreviewList = (props) => (
       tours.map(item => (
-      <TourPreviewListItem navigateToTourDetails={props.goToTourDetails} headline={item.headline} duration={item.duration} price={item.price} rating={item.rating} key={uuidv1()} />    
+      <TourPreviewListItem navigateToTourDetails={props.goToTourDetails} headline={item.headline} duration={item.duration} price={item.price} rating={item.rating} ratingNum={this.generateRandomNumer()} key={uuidv1()} />    
   )) 
 )
 
