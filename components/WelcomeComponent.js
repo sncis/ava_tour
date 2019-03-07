fe25623464505a56f691cdeced1a7db6b0a50e89
Krzysztof Ibek <krzysztof.ibek@dpschool.io>
2019-03-07 17:32:41 +0100

    fix of StartScreen

--- components/WelcomeComponent.js
+++ components/WelcomeComponent.js
@@ -1,13 +1,28 @@
 import React, { Component }from 'react';
-import { StyleSheet , View, Button, Text, Image } from 'react-native';
+import { StyleSheet , View, Button, Text, Image, ImageBackground } from 'react-native';
 
 export default class WelcomeComponent extends Component {
   render(){
     return(
-      <View>
-        <Text style={styles.welcomeText}>Welcome</Text>
-        <Button title="lets Start" onPress={this.props.goToTourOptions} />
+    <ImageBackground source={require("../assets/images/welcome_background.png")} 
+     style={{width: '100%', height: '100%'}}>
+      <View
+        style= {{ flex:1, flexDirection: 'column', justifyContent: 'space-evenly'}}
+        >
+        <View >
+          <Text style={styles.welcomeText}>
+            Welcome to
+          </Text>
+        </View>
+        <View>
+          <Image source = {require("../assets/images/logo-400.png")}> 
+          </Image>
+        </View>
+        <View>
+          <Button title="let's Start" onPress={this.props.goToTourOptions} />
+        </View>
     </View>
+    </ImageBackground>
     )
   }
 }
@@ -22,10 +37,12 @@ const styles = StyleSheet.create({
   },
   startButton:{
     marginTop: 100,
+    width: '90%',
     color: "blue",
   },
   avaLogo:{
-    resizeMode: 'stretch'
+    resizeMode: 'stretch',
+    width: '80%',
     
   },
   
