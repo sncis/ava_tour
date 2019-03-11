811f490876dcb6aa35974cd96d93c387ac0c1249
Krzysztof Ibek <krzysztof.ibek@dpschool.io>
2019-03-11 18:08:53 +0100

    changes on Database.js

--- components/TourPreviewList.js
+++ components/TourPreviewList.js
@@ -2,19 +2,43 @@ import React from 'react';
 import uuidv1 from "uuid";
 import { tours } from '../data';
 import TourPreviewListItem from './TourPreviewListItem';
-
+import {Database} from '../database/Database';
 
 const ratingNum = Math.floor(Math.random() * 5 )
 
 generateRandomNumer = () => {
   const ratingNum = Math.floor(Math.random() * 5 + 1 )
 }
-const TourPreviewList = (props) => (
-      tours.map(item => (
-      <TourPreviewListItem navigateToTourDetails={props.goToTourDetails} headline={item.headline} duration={item.duration} price={item.price} rating={item.rating} ratingNum={this.generateRandomNumer()} key={uuidv1()} />    
-  )) 
-)
 
-export default TourPreviewList;
+export default class RouteData extends React.Component {
+  constructor(props){
+    super(props)
+  }
 
+  onTourSelect(db, tourName){
+    db.setCurrentRouteName(tourName);
+    return props.goToTourDetails;
+  }
 
+  createRoutes(){
+    var db = Database.getInstance();
+    var routes = db.getAllTours('munic');
+    return routes.map(tour=>{
+      <TourPreviewListItem 
+        navigateToTourDetails= {this.onTourSelect(db,tour.tourName)} 
+        headline={tour.tourName} 
+        duration={tour.duration} 
+        price={tour.price} 
+        rating={tour.rating} 
+        ratingNum={this.generateRandomNumer()} 
+        key={uuidv1()} />
+    });
+  }
+
+  render() {
+    return(
+      this.createRoutes()
+    )
+  }
+  
+}
