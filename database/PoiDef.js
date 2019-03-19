4ae0bbb40beef084f27bc34da9175b5f204a1568
Akarsha Sehwag <akarsha15010@iiitd.ac.in>
2019-03-19 16:36:02 +0100

    Update PoiDef.js

--- database/PoiDef.js
+++ database/PoiDef.js
@@ -4,7 +4,229 @@
 export const hardcodedPoiDefs=()=>{
     
     return [
-       {
+        { 
+stayTime : 3.5*60*60,
+poiName : 'Alexander Krist Theatre',
+poiLocation: null,
+poiAdress : "Unterer Anger 3, 80331 Munich, Bavaria, Germany" ,
+poiDescription : "Das weltweit erste Table Magic Theater wurde im Oktober 2007 unter dem Namen „Zaubertheater München“ von gegründet. Seit Dezember 2009 residiert das Round Table Theater im Anger Palais zwischen Viktualienmarkt und Sendlinger Tor. Einzigartig ist die Anordnung der insgesamt 84 Sitzplätze: In fünf aufsteigenden Reihen nehmen die Gäste im Stil eines antiken Amphitheaters im Halbrund um den Tisch des Magiers Platz. 
+poiBriefly : "Das weltweit erste Table Magic Theater wurde im Oktober 2007 unter dem" ,
+poiType : "Events" ,
+ranking : 1,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'Bayerische Staatsoper',
+poiLocation: null,
+poiAdress : "Max-Joseph-Platz 2, 80539 Munich, Bavaria, Germany" ,
+poiDescription : "The best place in the city to view opera and ballet, although it can be a tough ticket. The interior of this theatre is all you could expect from an opera house. It’s charm is complimented by outstanding refreshment service with many types of food and drink available. A very large cloakroom caters for winter clothes and there is parking in the square outside. It’s always a memorable experience.
+poiBriefly : "The best place in the city to view opera and ballet, although it can b" ,
+poiType : "Events" ,
+ranking : 2,
+ },
+{ 
+stayTime : 3.0*60*60,
+poiName : 'Altes Residenztheatre',
+poiLocation: null,
+poiAdress : "Residenzstr. 1 | Innenstadt, 80333 Munich, Bavaria, Germany" ,
+poiDescription : "this glorious theater is very picturesque and stylish. One of these hidden gems not too many visitors is able to see.
+poiBriefly : "this glorious theater is very picturesque and stylish. One of these hi" ,
+poiType : "Events" ,
+ranking : 3,
+ },
+{ 
+stayTime : 3.0*60*60,
+poiName : 'Munich Philharmonic',
+poiLocation: null,
+poiAdress : "Rosenheimer Str. 5, 81667 Munich, Bavaria, Germany" ,
+poiDescription : "The city's most prominent orchestra plays in the Gasteig Kulturzentrum, one of the country's most modern concert halls.
+poiBriefly : "The city's most prominent orchestra plays in the Gasteig Kulturzentrum" ,
+poiType : "Events" ,
+ranking : 4,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'Ristorantino il cortile',
+poiLocation: null,
+poiAdress : "Furstenriederstrasse 8 | Eingang in hinterhof, 80687 Munich, Bavaria, Germany" ,
+poiDescription : "Italian, Pizza, Mediterranean, European, Vegetarian Friendly, Vegan Options. People enjoy 
+Pasta, pizza, and wine. Price range: €10-€30.
+poiBriefly : "Italian, Pizza, Mediterranean, European, Vegetarian Friendly, Vegan Op" ,
+poiType : "Food" ,
+ranking : 3,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'Chopan Shwabing',
+poiLocation: null,
+poiAdress : "Occamstr. 3, 80802 Munich, Bavaria, Germany" ,
+poiDescription : "Middle Eastern, Afghani, Vegetarian Friendly, Vegan Options, Halal. People specifically like lamb and chicken dishes here. Price range: €15-20
+poiBriefly : "Middle Eastern, Afghani, Vegetarian Friendly, Vegan Options, Halal. Pe" ,
+poiType : "Food" ,
+ranking : 1,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'Risotto Restaurant Munich',
+poiLocation: null,
+poiAdress : "Hirschgartenallee 38 | Nymphenburg Castle , Romanplatz, 80639 Munich, Bavaria, Germany" ,
+poiDescription : "Italian, Seafood, International, Mediterranean, Vegetarian Friendly. People claim to have had the best risotto in the world here !!
+poiBriefly : "Italian, Seafood, International, Mediterranean, Vegetarian Friendly. P" ,
+poiType : "Food" ,
+ranking : 2,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'La Boheme',
+poiLocation: null,
+poiAdress : "Leopoldstr. 180 | 80804, Munich, 80804 Munich, Bavaria, Germany" ,
+poiDescription : "French, Steakhouse, European, Grill, Vegetarian Friendly, Vegan Options. People claim to enjoy beef tartare here. Price range: €15 - €50
+
+poiBriefly : "French, Steakhouse, European, Grill, Vegetarian Friendly, Vegan Option" ,
+poiType : "Food" ,
+ranking : 4,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'Deutsches Meuseum',
+poiLocation: null,
+poiAdress : "Museum Island 1, 80538 Munich" ,
+poiDescription : "A science and technology museum like no other (according to travelers), the Deutsches Museum brims with exhibits on everything from transportation to mining, bridge building and musical instruments. 
+poiBriefly : "A science and technology museum like no other (according to travelers)" ,
+poiType : "Museums" ,
+ranking : 2,
+ },
+{ 
+stayTime : 2.5*60*60,
+poiName : 'Lenbachhaus',
+poiLocation: null,
+poiAdress : "Luisenstr. 33, 80333 Munich, Bavaria, Germany" ,
+poiDescription : "The Stadtische Galerie im Lenbachhaus was established in the former residence of the artist Franz von Lenbach and is most known for its world-class and unique collection of works by the artists of The Blue Rider, such as Franz Marc, Wassily Kandinsky, Paul Klee and Gabriele Munter, accented by eminent works of the Art Nouveau and New Objectivity, the art of the 19th century as well as international contemporary art.
+poiBriefly : "The Stadtische Galerie im Lenbachhaus was established in the former re" ,
+poiType : "Museums" ,
+ranking : 1,
+ },
+{ 
+stayTime : 1.5*60*60,
+poiName : 'Munich Residence',
+poiLocation: null,
+poiAdress : "Residenzstr. 1, 80333 Munich, Bavaria, Germany" ,
+poiDescription : "The Munich Residence served as the seat of government and residence of the Bavarian dukes, electors and kings from 1508 to 1918. The rooms and art collections spanning a period that begins with the Renaissance, and extends via the early Baroque and Rococo epochs to Neoclassicism, bear witness to the discriminating taste and the political ambition of the Wittelsbach dynasty.
+poiBriefly : "The Munich Residence served as the seat of government and residence of" ,
+poiType : "Museums" ,
+ranking : 3,
+ },
+{ 
+stayTime : 3.5*60*60,
+poiName : 'Die Pinakotheken',
+poiLocation: null,
+poiAdress : "Barer Strasse 27 | Eingang Theresienstraße, 80333 Munich, Bavaria, Germany" ,
+poiDescription : "Housing much of the city's most famous artwork, this museum's collection includes renowned international works from the 14th through the 18th centuries.
+poiBriefly : "Housing much of the city's most famous artwork, this museum's collecti" ,
+poiType : "Museums" ,
+ranking : 4,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'Englisch Garden',
+poiLocation: null,
+poiAdress : "englisch garten, munich" ,
+poiDescription : "This large public park, which reaches from the city center to the northeastern city limits, is one of Munich's must-dos.
+poiBriefly : "This large public park, which reaches from the city center to the nort" ,
+poiType : "Parks" ,
+ranking : 1,
+ },
+{ 
+stayTime : 2.0*60*60,
+poiName : 'Olympiapark',
+poiLocation: null,
+poiAdress : "Spiridon-Louis-Ring 21 | Am Riesenfeld, 80809 Munich, Bavaria, Germany" ,
+poiDescription : "See Olympiapark, home of the 1972 Olympics. Tours include a roof climb, with ropes & carbiners and bungee jumping into the stadium.
+poiBriefly : "See Olympiapark, home of the 1972 Olympics. Tours include a roof climb" ,
+poiType : "Parks" ,
+ranking : 3,
+ },
+{ 
+stayTime : 3.5*60*60,
+poiName : 'Tierpark Hellabrunn',
+poiLocation: null,
+poiAdress : "Tierparkstr. 30, 81543 Munich, Bavaria, Germany" ,
+poiDescription : "Huge complex of buildings makes this one of the most enjoyable museums you will ever visit. Plan to spend a great portion of the day there since there are so many exhibits and rooms to see.
+poiBriefly : "Huge complex of buildings makes this one of the most enjoyable museums" ,
+poiType : "Parks" ,
+ranking : 2,
+ },
+{ 
+stayTime : 3.5*60*60,
+poiName : 'Viktualienmakt',
+poiLocation: null,
+poiAdress : "Viktualienmarkt 3, 80331 Munich, Bavaria, Germany" ,
+poiDescription : "The city's largest and most famous outdoor marketplace
+poiBriefly : "The city's largest and most famous outdoor marketplace" ,
+poiType : "Shopping" ,
+ranking : 1,
+ },
+{ 
+stayTime : 3.5*60*60,
+poiName : 'Style Advisor Twins',
+poiLocation: null,
+poiAdress : "80333 Munich, Bavaria, Germany" ,
+poiDescription : "Style Advisor Twins is based in Munich, Germany. Our key services: Personal Styling, Personal Shopping and Personal Branding. Travelers enjoy effortless shopping in Munich with us. We magically deliver your favourite new pieces while always remaining true to your style and leaving you time to enjoy the exciting world of art, architecture & local Cuisine in Munich.
+poiBriefly : "Style Advisor Twins is based in Munich, Germany. Our key services: Per" ,
+poiType : "Shopping" ,
+ranking : 2,
+ },
+{ 
+stayTime : 1.5*60*60,
+poiName : 'Kunsthalle',
+poiLocation: null,
+poiAdress : "Theatinerstr. 8, 80333 Munich, Bavaria, Germany" ,
+poiDescription : "Nestled between Marienplatz and Odeonsplatz in the heart of Munich, the Kunsthalle organises three exhibitions annually and has built up an international reputation. Every year, the Kunsthalle welcomes around 350,000 visitors and is one of Germany's leading exhibition venues. Art and culture from around the world are displayed here in all their glory, whether it be painting, sculpture, graphic arts, photography, handcrafts or design.
+poiBriefly : "Nestled between Marienplatz and Odeonsplatz in the heart of Munich, th" ,
+poiType : "Shopping" ,
+ranking : 3,
+ },
+{ 
+stayTime : 2.5*60*60,
+poiName : 'Galeria Kaufhof',
+poiLocation: null,
+poiAdress : "Kaufingerstr. 1-5, 80331 Munich, Bavaria, Germany" ,
+poiDescription : "Galeria Kaufhof is a mall covering all life goods. It goes from clothes for all the family to toys through bags. Price are correct. Sales are interesting. Decent selection with a mini Sephora and other mid-end selection like Bare Minerals. The set up of the Galeria is more pop-up style.
+poiBriefly : "Galeria Kaufhof is a mall covering all life goods. It goes from clothe" ,
+poiType : "Shopping" ,
+ranking : 4,
+ },
+{ 
+stayTime : 1.0*60*60,
+poiName : 'Church of our lady',
+poiLocation: null,
+poiAdress : "Frauenplatz 12, 80331 München" ,
+poiDescription : "The gothic-period Frauenkirche, or Church of our Lady, is a unique part of the Munich skyline with its two dome-topped towers, which residents say resembles a pair of beer steins bubbling over. 
+poiBriefly : "The gothic-period Frauenkirche, or Church of our Lady, is a unique par" ,
+poiType : "Sightseeing" ,
+ranking : 2,
+ },
+{ 
+stayTime : 1.0*60*60,
+poiName : 'St. Peter's Church',
+poiLocation: null,
+poiAdress : "Rindermarkt 1, 80331 München" ,
+poiDescription : "Open daily, Peterskirche (St. Peter's Church) is Munich's oldest and smallest place of worship. Recent travelers were impressed by the church's glittering altar, not to mention the gold, jewel-bedecked skeleton of St. Munditia, a revered Christian martyr. 
+poiBriefly : "Open daily, Peterskirche (St. Peter's Church) is Munich's oldest and s" ,
+poiType : "Sightseeing" ,
+ranking : 3,
+ },
+{ 
+stayTime : 1.5*60*60,
+poiName : 'Marienplatz',
+poiLocation: null,
+poiAdress : "Marienplatz 1, 80331 München" ,
+poiDescription : "The Marienplatz square, accessible by a U-bahn station of the same name, is Munich's heart just as it used to be back in 1158 when it was established. Back then, it hosted spectator events, such as jousts and executions (yikes!). Today this Alstadt (Old Town) square is alive with street performers, from mimes to musicians, and restaurants, selling bratwursts and beer, plus lots of surrounding shops.
+poiBriefly : "The Marienplatz square, accessible by a U-bahn station of the same nam" ,
+poiType : "Sightseeing" ,
+ranking : 1,
+ }
+        ,{
            stayTime        : 90*60, 
            poiName         : 'BMW World',
            poiLocation     : "48.177016,11.5565994",
